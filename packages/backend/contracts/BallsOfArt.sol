304acb28891c906b1d5970b45e4ff3a01ac97b1d
Radek Sienkiewicz <mail@velvetshark.com>
2022-09-16 01:28:37 +0300

    Ball size drawing, with probabilities

--- packages/backend/contracts/BallsOfArt.sol
+++ packages/backend/contracts/BallsOfArt.sol
@@ -6,6 +6,7 @@ pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract BallsOfArt is ERC721, Ownable {
     // Ball parameters
@@ -18,8 +19,40 @@ contract BallsOfArt is ERC721, Ownable {
         uint256 randomBase;
     }
 
-    string[] internal backgroundColors = ["#ffffff", "#fafafa"];
-    string[] internal ballColors = ["#00ff00", "#ff0000", "#0000ff"];
+    string[7] internal backgroundColors = [
+        "#ffffff",
+        "#F1F1F1",
+        "#F7ECDE",
+        "#EEF6FF",
+        "#FCF8E8",
+        "#FFF9D7",
+        "#F9F2ED"
+    ];
+    string[23] internal ballColors = [
+        "#00ff00",
+        "#ff0000",
+        "#0000ff",
+        "#367E18",
+        "#F57328",
+        "#CC3636",
+        "#D800A6",
+        "#400D51",
+        "#31E1F7",
+        "#FA7070",
+        "#A1C298",
+        "#8758FF",
+        "#F07DEA",
+        "#25316D",
+        "#3D8361",
+        "#FECD70",
+        "#FFC4C4",
+        "#C3FF99",
+        "#F94892",
+        "#FF7F3F",
+        "#2A0944",
+        "#3330E4",
+        "#F637EC"
+    ];
 
     uint256 public maxSupply = 111; // max number of tokens
     uint256 public mintedTokens = 0; // number of tokens minted
@@ -43,7 +76,7 @@ contract BallsOfArt is ERC721, Ownable {
                 y: y,
                 width: width,
                 height: height,
-                fill: backgroundColors[(randomBase % 50)], // Choose random color from backgroundColors array
+                fill: ballColors[(randomBase % 21)], // Choose random color from backgroundColors array. TODO: Change to 50 (size of bgcolors array)
                 randomBase: randomBase
             });
     }
@@ -62,7 +95,7 @@ contract BallsOfArt is ERC721, Ownable {
                 '" fill="',
                 ball.fill,
                 '" rx="150" /> <path fill="none" stroke="#ffffff" stroke-width="20" stroke-linecap="round" d="M ',
-                uint2str(ball.x + 150),
+                uint2str(ball.x + ball.width - 150),
                 " ",
                 uint2str(ball.y + ball.height - 50),
                 " A 100 100 0 0 0 ",
@@ -74,16 +107,16 @@ contract BallsOfArt is ERC721, Ownable {
     }
 
     // TODO review all randomBases and shift accordingly
-    function generateLineSvg(uint256 lineNumber)
+    function generateLineSvg(uint256 lineNumber, uint256 randomBase)
         public
         view
         returns (bytes memory)
     {
-        // A base for all "random" values
-        uint256 randomBase = uint256(
-            keccak256(abi.encodePacked(blockhash(block.number - 1)))
-        );
-        // Final SVG
+        // // A base for all "random" values
+        // uint256 randomBase = uint256(
+        //     keccak256(abi.encodePacked(blockhash(block.number - 1)))
+        // );
+        // Line SVG
         bytes memory lineSvg = "";
 
         uint256 y = 150; // Default y for row 1
@@ -95,6 +128,7 @@ contract BallsOfArt is ERC721, Ownable {
 
         // Size of ball at slot 1
         uint256 ballSize11 = drawBallSize(3, randomBase);
+        console.log("Ball size 1: ", ballSize11);
 
         // Ball size 1x? Paint 1x at slot 1
         if (ballSize11 == 1) {
@@ -103,7 +137,8 @@ contract BallsOfArt is ERC721, Ownable {
 
             // Slot 2
             // Size of ball at slot 2
-            uint256 ballSize12 = drawBallSize(3, randomBase);
+            uint256 ballSize12 = drawBallSize(2, randomBase >> 2);
+            console.log("Ball size 2: ", ballSize12);
 
             // Ball size 1x? Paint 1x at slot 2 and 1x at slot 3
             if (ballSize12 == 1) {
@@ -112,14 +147,14 @@ contract BallsOfArt is ERC721, Ownable {
                     y,
                     300,
                     300,
-                    randomBase >> 1
+                    randomBase >> 4
                 );
                 Ball memory ball13 = createBallStruct(
                     800,
                     y,
                     300,
                     300,
-                    randomBase >> 2
+                    randomBase >> 6
                 );
                 lineSvg = bytes.concat(
                     lineSvg,
@@ -134,28 +169,42 @@ contract BallsOfArt is ERC721, Ownable {
                     y,
                     625,
                     300,
-                    randomBase
+                    randomBase >> 8
                 );
                 lineSvg = bytes.concat(lineSvg, ballSvg(ball12));
             }
 
             // Ball size 2x? Paint 2x at slot 1 and 1x at slot 3
         } else if (ballSize11 == 2) {
-            Ball memory ball11 = createBallStruct(150, y, 300, 625, randomBase);
+            Ball memory ball11 = createBallStruct(
+                150,
+                y,
+                625,
+                300,
+                randomBase >> 10
+            );
             Ball memory ball13 = createBallStruct(
                 800,
                 y,
                 300,
                 300,
-                randomBase >> 1
+                randomBase >> 12
             );
             lineSvg = bytes.concat(lineSvg, ballSvg(ball11), ballSvg(ball13));
 
             // Ball size 3x? Paint 3x at slot 1
         } else if (ballSize11 == 3) {
-            Ball memory ball11 = createBallStruct(150, y, 950, 300, randomBase);
+            Ball memory ball11 = createBallStruct(
+                150,
+                y,
+                950,
+                300,
+                randomBase >> 14
+            );
             lineSvg = bytes.concat(lineSvg, ballSvg(ball11));
         }
+        console.log("lineSvg: ", string(lineSvg));
+        return lineSvg;
     }
 
     function drawBallSize(uint256 maxSize, uint256 randomBase)
@@ -163,7 +212,31 @@ contract BallsOfArt is ERC721, Ownable {
         pure
         returns (uint256 size)
     {
-        return size = (randomBase % maxSize) + 1;
+        // Random number 1-100
+        uint256 r = (randomBase % 100) + 1;
+
+        // Probabilities:
+        // 3x: 16%
+        // 2x: 32%
+        // else: 1x
+        if (maxSize == 3) {
+            if (r <= 16) {
+                return 3;
+            } else if (r <= 48) {
+                return 2;
+            } else {
+                return 1;
+            }
+        } else {
+            // Probabilities:
+            // 2x: 30%
+            // else: 1x
+            if (r <= 30) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }
     }
 
     // From: https://stackoverflow.com/a/65707309/11969592
